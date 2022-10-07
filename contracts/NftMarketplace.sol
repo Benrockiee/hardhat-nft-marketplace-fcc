e7a0ac41a8fa19e40b3ed7823516394fcf845b17
Benjamin izuchukwu umeike <benrockiee@gmail.com>
2022-10-07 03:13:31 -0700

    Comments

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -41,15 +41,21 @@ contract NftMarketplace is ReentrancyGuard {
         uint256 indexed tokenId,
         uint256 price
     );
-
+    /*mapping of NFT addresses mapped to a tokenId, mapped to listings 
+    so back in our listing function, we update this listings */
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+    /* This mapping keeps track of how much money they make selling their NFTS*/
     mapping(address => uint256) private s_proceeds;
 
+    /* To make sure we dont relist NFTS that are already listed, we 
+       make this modidfier */
     modifier notListed(
         address nftAddress,
         uint256 tokenId,
         address owner
     ) {
+        /*We make a new listing here & when we are done, we add it to 
+        our list item function*/
         Listing memory listing = s_listings[nftAddress][tokenId];
         if (listing.price > 0) {
             revert AlreadyListed(nftAddress, tokenId);
@@ -57,19 +63,26 @@ contract NftMarketplace is ReentrancyGuard {
         _;
     }
 
+    /* To make sure that buyItem is already listed,
+       we make an isListed modifier */
     modifier isListed(address nftAddress, uint256 tokenId) {
+        /*to check if this is listed we do: */
         Listing memory listing = s_listings[nftAddress][tokenId];
+        /*we check the price*/
         if (listing.price <= 0) {
             revert NotListed(nftAddress, tokenId);
         }
         _;
     }
-
+    /* To make sure that these listed NFTS are owned by the msg.sender,
+       we make an isOwner modifier */
     modifier isOwner(
         address nftAddress,
         uint256 tokenId,
         address spender
     ) {
+        /*We make this here & when we are done, we add it to 
+        our list item function*/
         IERC721 nft = IERC721(nftAddress);
         address owner = nft.ownerOf(tokenId);
         if (spender != owner) {
@@ -93,18 +106,50 @@ contract NftMarketplace is ReentrancyGuard {
         uint256 price
     )
         external
+        /*notListed Modifier appended here*/
         notListed(nftAddress, tokenId, msg.sender)
+        /*isOwner Modifier appended here*/
         isOwner(nftAddress, tokenId, msg.sender)
     {
         if (price <= 0) {
             revert PriceMustBeAboveZero();
         }
+
+        /*In other for us to list this, we can do this in one or two ways
+    1. We can send the NFTs to the contract & this would require a transfer and this
+       means the contract holding the NFT.abi but this is Gas expensive and also 
+       we will have the market hold our NFTand the user wont technically own it,
+       infact they can, but they will have to withdraw it OR we can :
+
+    2. owners can still hold their NFT and give the marketplace approval to sell
+       the NFTS for them once their prices are met. so to make sure the marketPlace 
+       has approval, we import /ERC721 from openzeppelin to use the approval function
+       and we can call "getApproved" on that address.. 
+       so now that we have this interface right here, we can say:
+     /*/
+
         IERC721 nft = IERC721(nftAddress);
+        /* If we are not approved, we revert */
         if (nft.getApproved(tokenId) != address(this)) {
             revert NotApprovedForMarketplace();
         }
+        /* Now we probably wanna have some type of data structures 
+        to list this NFT, if we use an array, anytime someone wants
+        to buy an item, we have to traverse through it or make this
+        massive dynamic array and it wouldnt be good as that gets
+        really really big so we make this a MAPPING, its above */
+        /* when we are done with that mapping, we update that s_listings mappings
+       
+          s_listings of the address, at the tokenId   */
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        /* Since we are updating the mapping, its a good practice to 
+         emit an event especially for this project and alson define
+         at the top, SEE TOP */
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+        /* To make sure we dont relist NFTS that are already listed, we 
+       make a notListed modidfier, SEE TOP */
+        /* To make sure that these listed NFTS are owned by the msg.sender,
+       we make an isOwner modifier, SEE TOP */
     }
 
     /*
@@ -114,10 +159,14 @@ contract NftMarketplace is ReentrancyGuard {
      */
     function cancelListing(address nftAddress, uint256 tokenId)
         external
+        /* Only the owner of this NFT can cancel it */
         isOwner(nftAddress, tokenId, msg.sender)
+        /* To make sure the NFT is actually listed */
         isListed(nftAddress, tokenId)
     {
+        /* To cancel the listings(that mapping) we do: */
         delete (s_listings[nftAddress][tokenId]);
+        /* We emit the event */
         emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
 
@@ -135,19 +184,30 @@ contract NftMarketplace is ReentrancyGuard {
         isListed(nftAddress, tokenId)
         nonReentrant
     {
+        /* To make sure that buyItem is already listed,
+       we make an isListed modifier, SEE TOP */
+
         // Challenge - How would you refactor this contract to take:
         // 1. Abitrary tokens as payment? (HINT - Chainlink Price Feeds!)
         // 2. Be able to set prices in other currencies?
         // 3. Tweet me @PatrickAlphaC if you come up with a solution!
         Listing memory listedItem = s_listings[nftAddress][tokenId];
+        //To make sure they are sending us enough money, we do:
         if (msg.value < listedItem.price) {
             revert PriceNotMet(nftAddress, tokenId, listedItem.price);
         }
+        /* When somebody buys an item, this will update their proceeds*/
+        /* We dont just send the seller money, we have them withdraw it,
+         so we push the risk to them */
+
         s_proceeds[listedItem.seller] += msg.value;
-        // Could just send the money...
+
         // https://fravoll.github.io/solidity-patterns/pull_over_push.html
+        /* Once we buy this item, we delete the listings from the market place: */
         delete (s_listings[nftAddress][tokenId]);
+        /*So we go ahead and transfer it */
         IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+        /*Since we are updating a mapping, lets emit an event*/
         emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
     }
 
@@ -163,6 +223,7 @@ contract NftMarketplace is ReentrancyGuard {
         uint256 newPrice
     )
         external
+        /* We make sure its listed */
         isListed(nftAddress, tokenId)
         nonReentrant
         isOwner(nftAddress, tokenId, msg.sender)
@@ -171,18 +232,22 @@ contract NftMarketplace is ReentrancyGuard {
         if (newPrice <= 0) {
             revert PriceMustBeAboveZero();
         }
+        /*To  update our listings */
         s_listings[nftAddress][tokenId].price = newPrice;
+        /* We emit itemListed */
         emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
     }
 
     /*
      * @notice Method for withdrawing proceeds from sales
      */
+    /*We are getting all the payments that we collected in buy item */
     function withdrawProceeds() external {
         uint256 proceeds = s_proceeds[msg.sender];
         if (proceeds <= 0) {
             revert NoProceeds();
         }
+        /*We reset the proceeds to 0 */
         s_proceeds[msg.sender] = 0;
         (bool success, ) = payable(msg.sender).call{value: proceeds}("");
         require(success, "Transfer failed");
@@ -204,3 +269,4 @@ contract NftMarketplace is ReentrancyGuard {
         return s_proceeds[seller];
     }
 }
+
